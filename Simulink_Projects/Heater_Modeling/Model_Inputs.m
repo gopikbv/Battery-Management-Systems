ba2b0d0ddd589a4a7eb4652c9d716ba175ab06f7
gopikbv <gopinathmpt@gmail.com>
2024-09-07 23:57:39 +0530

    Heater Model

---
+++ Simulink_Projects/Heater_Modeling/Model_Inputs.m
@@ -0,0 +1,20 @@
+% House
+% Thermal Resistance
+Req = 0.0000004; %c/j-hr
+
+% Mass of air
+M=5987.7684; %kg
+
+%Specific heat capacity of air
+c=1005.4; %j/kg-c
+
+%Cost
+cost=2.5/(3.6*10^6); %rs/j
+
+
+%Heater
+%Mass of air through the heater
+Mdot=3600; %kh/hr
+
+%Heater temperature
+Theater=50; %Degree C
